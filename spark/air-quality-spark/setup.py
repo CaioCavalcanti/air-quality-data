f9cb3185b93d6a39afc79d768910adb516fe7c3d
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-03 22:12:22 +0200

    test bump version 0.0.3

--- spark/air-quality-spark/setup.py
+++ spark/air-quality-spark/setup.py
@@ -7,7 +7,7 @@ setup(
     name="air-quality-spark",
     # ideally this should be handled automatically with gitversion
     # but for simplicity we will do it manually.
-    version="0.0.2",
+    version="0.0.3",
     install_requires=["pyspark==3.1.3", "delta-spark==1.0.1"],
     packages=["air_quality_spark"],
     package_dir={"air_quality_spark": "src/air_quality_spark"},
