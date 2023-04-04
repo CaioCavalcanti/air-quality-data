bcf12dcd3f3c5ba59669dbf45cef3d1c23731982
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-04 23:14:28 +0200

    bump orchestration package version for test

--- prefect/air-quality-orchestration/setup.py
+++ prefect/air-quality-orchestration/setup.py
@@ -6,7 +6,7 @@ setup(
     name="air-quality-orchestration",
     # ideally this should be handled automatically with gitversion
     # but for simplicity we will do it manually.
-    version="0.0.1",
+    version="0.0.2",
     install_requires=["prefect-gcp[cloud_storage]==0.3.0"],
     packages=["air_quality_orchestration"],
     package_dir={"air_quality_orchestration": "src/air_quality_orchestration"},
