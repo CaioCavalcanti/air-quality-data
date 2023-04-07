ed22d6ecf7a5e9637cc45a636ee48fc6cc2d56fc
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-07 20:12:38 +0200

    enforce prefect version 2.9.0

--- prefect/air-quality-orchestration/setup.py
+++ prefect/air-quality-orchestration/setup.py
@@ -2,12 +2,15 @@
 """
 from setuptools import setup
 
+with open("requirements.txt", encoding="utf-8") as requirements:
+    install_requires = requirements.read().strip().split("\n")
+
 setup(
     name="air-quality-orchestration",
     # ideally this should be handled automatically with gitversion
     # but for simplicity we will do it manually.
-    version="0.0.2",
-    install_requires=["prefect-gcp[cloud_storage]==0.4.0"],
+    version="0.0.3",
+    install_requires=install_requires,
     packages=["air_quality_orchestration"],
     package_dir={"air_quality_orchestration": "src/air_quality_orchestration"},
 )
