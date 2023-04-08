90a5b19b9af6bdc208d86197636d19e6e32db235
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-08 23:15:24 +0200

    implement Spark.submit_job method

--- prefect/air-quality-orchestration/src/air_quality_orchestration/prefect.py
+++ prefect/air-quality-orchestration/src/air_quality_orchestration/prefect.py
@@ -1,5 +1,7 @@
 """Provide utilities for orchestration via Prefect, such as custom blocks.
 """
+from google.cloud.dataproc_v1 import JobControllerClient
+from google.cloud.dataproc_v1.types import Job
 from google.cloud.storage import Client, Bucket
 from prefect.blocks.core import Block
 
@@ -28,19 +30,43 @@ class Spark(Block):
         blob = spark_jobs_bucket.blob(blob_path)
         blob.upload_from_filename(local_path)
 
-        blob_link = f"gs://{spark_jobs_bucket.name}/{blob_path}"
+        blob_link = self._get_job_url(destination_path)
 
         return blob_link
 
-    def submit_job(self, job_path, args):
+    def submit_job(self, job_path, args) -> Job:
         """Submits the Spark job on the given `job_path` to be executed in the Dataproc cluster.
 
         Args:
             job_path (_type_): the URL for the Spark job on the GCS bucket.
             args (_type_): the arguments to pass when submitting the job.
         """
-        raise NotImplementedError
+        job_client = JobControllerClient(
+            client_options={
+                "api_endpoint": f"{self.gcp_region}-dataproc.googleapis.com:443"
+            }
+        )
+
+        job_uri = self._get_job_url(job_path)
+
+        job = {
+            "placement": {"cluster_name": self.gcp_dataproc_cluster_name},
+            "pyspark_job": {"main_python_file_uri": job_uri, "args": args},
+        }
+
+        operation = job_client.submit_job_as_operation(
+            request={
+                "project_id": self.gcp_project_id,
+                "region": self.gcp_region,
+                "job": job,
+            }
+        )
+
+        return operation.result()
 
     def _get_spark_jobs_bucket(self) -> Bucket:
         client = Client()
         return client.get_bucket(self.gcp_gcs_spark_jobs_bucket_name)
+
+    def _get_job_url(self, job_relative_path: str) -> str:
+        return f"gs://{self.gcp_gcs_spark_jobs_bucket_name}/jobs/{job_relative_path}"
