4b1c60f24d3ba274c34f320903fefa23df0f3580
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-09 08:51:29 +0200

    give prefect agent sa access Dataproc

--- terraform/prefect.tf
+++ terraform/prefect.tf
@@ -30,3 +30,15 @@ resource "google_artifact_registry_repository_iam_member" "prefect_agent_service
   role       = "roles/artifactregistry.reader"
   member     = google_service_account.prefect_agent_service_account.member
 }
+
+resource "google_project_iam_member" "prefect_agent_service_account_dataproc_worker" {
+  project = var.gcp_project_id
+  role    = "roles/dataproc.editor"
+  member  = google_service_account.prefect_agent_service_account.member
+}
+
+resource "google_storage_bucket_iam_member" "prefect_agent_service_account_reader_on_spark_staging" {
+  bucket = google_storage_bucket.spark_staging.name
+  role   = "roles/storage.objectViewer"
+  member = google_service_account.prefect_agent_service_account.member
+}
