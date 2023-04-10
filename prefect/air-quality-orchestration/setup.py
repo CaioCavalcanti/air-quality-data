0278fc3c2afc523c48984d8c1430e8e49814b620
Caio Cavalcanti <caiofabiomc@gmail.com>
2023-04-10 16:02:42 +0200

    bump air-quality-orchestration to 0.0.5

--- prefect/air-quality-orchestration/setup.py
+++ prefect/air-quality-orchestration/setup.py
@@ -9,7 +9,7 @@ setup(
     name="air-quality-orchestration",
     # ideally this should be handled automatically with gitversion
     # but for simplicity we will do it manually.
-    version="0.0.4",
+    version="0.0.5",
     install_requires=install_requires,
     packages=["air_quality_orchestration"],
     package_dir={"air_quality_orchestration": "src/air_quality_orchestration"},
